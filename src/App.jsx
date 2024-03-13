a5bfb28f859f22ebd05cdb26c0e2930061bc3140
manumn95 <manumnofficial.95@gmail.com>
2024-03-13 13:18:25 +0530

    Added the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import Card from "./components/pricecard";
+const App = () => {
+  const data = {
+    cards: [
+      {
+        type: "FREE",
+        amount: "$0/month",
+        features: [
+          "✔ Single User",
+          "✔ 50GB Storage",
+          "✔ Unlimited public projects",
+          "✔ Community Access",
+          "✖ Unlimited private projects",
+          "✖ Dedicated phone",
+          "✖ Free subdomain",
+          "✖ Monthly status Reports",
+        ],
+      },
+      {
+        type: "PLUS",
+        amount: "$9/month",
+        features: [
+          "✔ 5 Users",
+          "✔ 50GB Storage",
+          "✔ Unlimited public projects",
+          "✔ Community Access",
+          "✔ Unlimited private projects",
+          "✔ Dedicated phone",
+          "✔ Free subdomain",
+          "✖ Monthly status Reports",
+        ],
+      },
+      {
+        type: "PRO",
+        amount: "$49/month",
+        features: [
+          "✔ Unlimited Users",
+          "✔ 50GB Storage",
+          "✔ Unlimited public projects",
+          "✔ Community Access",
+          "✔ Unlimited private projects",
+          "✔ Dedicated phone",
+          "✔ Free subdomain",
+          "✔ Monthly status Reports",
+        ],
+      },
+    ],
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div className="d-flex justify-content-center align-item-center mt-5">
+      {data.cards.map((val, index) => {
+        return <Card data={val} key={index} features={val.features} />;
+      })}
+    </div>
+  );
+};
+export default App;
